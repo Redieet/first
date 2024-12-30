d80d7e2000337669c4d7756e6674c354f98b6b00
rediet <148014485+Redieet@users.noreply.github.com>
2024-12-30 15:16:23 -0500

    Time: 2 ms (71.04%) | Memory: 12.5 MB (12.66%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        INT_MIN, INT_MAX = -2**31, 2**31 - 1
+        i = 0
+        n = len(s)
+        while i < n and s[i] == ' ':
+            i += 1
+
+        if i == n:
+            return 0
+        sign = 1
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+        result = 0
+        while i < n and '0' <= s[i] <= '9':
+            digit = ord(s[i]) - ord('0') 
+            if result > (INT_MAX - digit) // 10:
+                return INT_MAX if sign == 1 else INT_MIN
+            result = result * 10 + digit
+            i += 1
+
+        return sign * result
